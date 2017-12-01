8924128095796291743e45e78500153d275e1ca0
Colin Pistell <jpiste@ext.uber.com>
2017-12-01 16:48:35 -0700

    initial commit

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(colinreverser)
+
+test_check("colinreverser")
---
+++ tests/testthat/test_reverser.R
@@ -0,0 +1,14 @@
+library(colinreverser)
+context("String reverser")
+
+test_that("Strings are reversed correctly", {
+              expect_equal(colin_reverser("foo"), "oof")
+              expect_equal(colin_reverser("happy day"), "yad yppah")
+              expect_equal(colin_reverser("x"), "x")
+})
+
+test_that("Numbers are reversed correctly", {
+              expect_equal(colin_reverser(1234), "4321")
+              expect_equal(colin_reverser(3.14), "41.3")
+              expect_equal(colin_reverser(5), "5")
+})
